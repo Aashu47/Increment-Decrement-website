3889a23e8dd5df312b7524f50aea4eab02457115
Aashu47 <aashu.05134@gmail.com>
2023-08-15 01:54:34 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+console.log('hlo')
+
+const countValue = document.querySelector('#counter');
+
+
+const decrement = () => {
+    let value = parseInt(countValue.innerText);
+    value = value - 1;
+    countValue.innerText = value;
+};
+
+
+const increment = () => {
+    let value = parseInt(countValue.innerText);
+    value = value +1;
+    countValue.innerText = value;
+};
